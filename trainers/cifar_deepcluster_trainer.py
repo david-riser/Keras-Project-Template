17e52477e046cca1e10852ea9326434fc81d9e21
Ubuntu <ubuntu@ip-172-31-23-155.ec2.internal>
2020-05-23 12:41:20 +0000

    A bit of refactoring never hurts

--- trainers/cifar_deepcluster_trainer.py
+++ trainers/cifar_deepcluster_trainer.py
@@ -3,7 +3,7 @@ import os
 
 from base.base_trainer import BaseTrain
 from sklearn.cluster import MiniBatchKMeans
-
+from sklearn.metrics import adjusted_rand_score
 
 class CifarDeepClusterTrainer(BaseTrain):
 
@@ -19,9 +19,47 @@ class CifarDeepClusterTrainer(BaseTrain):
         
     def train(self):
 
-        # First initialize the model cluster centroids.  This is
-        # done with kmeans.  Since the dataset it large, we use
-        # mini-batch kmeans. 
+        self.init_clusters_()        
+
+        print("[INFO] Starting the training.")
+        update_interval = int(np.ceil(self.config.trainer.batches / self.config.trainer.target_updates))
+        if update_interval == 0:
+            update_interval = 1        
+        print("[INFO] Using update interval {}".format(update_interval))
+    
+        kld_loss = []
+        ar_score = []
+        loss = np.inf
+        for iteration in range(self.config.trainer.batches):
+
+            if iteration % update_interval == 0:
+            
+                (x_batch, y_batch) = next(self.data.get_train_flow())
+                while len(x_batch) != self.config.data_loader.batch_size:
+                    (x_batch, y_batch) = next(self.data.get_train_flow())            
+
+                q = self.model.model.predict(x_batch, verbose=0)
+                p = self.clustering_target_distribution(q)
+
+
+            (x_batch, y_batch) = next(self.data.get_train_flow())
+            if len(x_batch) == self.config.data_loader.batch_size:
+                loss = self.model.model.train_on_batch(x=x_batch, y=p)
+                kld_loss.append(loss)
+                ar_score.append(adjusted_rand_score(
+                    y_batch.reshape(-1,),
+                    np.argmax(self.model.model.predict(x_batch), axis=1)
+                ))
+                
+            print("[INFO] Epoch: {0}, Loss: {1:8.4f}, AR Score {2:8.4f}".format(
+                iteration, loss, ar_score[-1]))
+            
+        self.loss = kld_loss
+        self.rand_index = ar_score
+
+
+    def init_clusters_(self):
+
         print("[INFO] Initializing cluster centroids w/ Kmeans (mini-batch).")
         if self.config.data_loader.batch_size < self.config.model.n_clusters:
             print("[FATAL] Mini-batch KMeans requires a batch size at least as large as the number of clusters.")
@@ -39,34 +77,3 @@ class CifarDeepClusterTrainer(BaseTrain):
         self.model.model.get_layer("clustering_layer").set_weights(
             [self.kmeans.cluster_centers_]
         )
-
-        print("[INFO] Done!  Starting the training.")
-        update_interval = int(np.ceil(self.config.trainer.batches / self.config.trainer.target_updates))
-        if update_interval == 0:
-            update_interval = 1
-        
-        print("[INFO] Using update interval {}".format(update_interval))
-    
-        kld_loss = []
-        loss = np.inf
-        for ite in range(self.config.trainer.batches):
-
-            if ite % update_interval == 0:
-            
-                (x_batch, y_batch) = next(self.data.get_train_flow())
-                while len(x_batch) != self.config.data_loader.batch_size:
-                    (x_batch, y_batch) = next(self.data.get_train_flow())            
-
-                q = self.model.model.predict(x_batch, verbose=0)
-                p = self.clustering_target_distribution(q)
-
-
-            (x_batch, y_batch) = next(self.data.get_train_flow())
-            if len(x_batch) == self.config.data_loader.batch_size:
-                loss = self.model.model.train_on_batch(x=x_batch, y=p)
-                kld_loss.append(loss)
-
-            print("[INFO] Epoch: {0}, Loss: {1:8.4f}".format(ite, loss))
-            
-        self.loss = kld_loss
-        
